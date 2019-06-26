bb95965b09149b50e0fee2e4f354f6840b882e59
Kevin Kuipers <nanark@gmail.com>
2019-06-26 12:10:01 +0200

    Refacto & comments

--- assets/scripts/game/GameLoop.js
+++ assets/scripts/game/GameLoop.js
@@ -5,37 +5,47 @@ import { Game } from "@/assets/scripts/game/Game";
 let tickerTime = 0;
 let previousPosition = [0, 0];
 
+// Send the player position if he moved
 const sendPosition = () => {
   const currentPosition = [Game.player.sprite.x, Game.player.sprite.y];
 
-  if (!isEqual(previousPosition, currentPosition)) {
-    previousPosition = currentPosition;
-
-    Game.ws.send(
-      JSON.stringify({
-        namespace: "position",
-        event_type: "message",
-        data: {
-          x: Game.player.sprite.x,
-          y: Game.player.sprite.y,
-          animation: Game.player.animation
-        }
-      })
-    );
+  if (isEqual(previousPosition, currentPosition)) {
+    return false;
   }
+
+  previousPosition = currentPosition;
+
+  const position = {
+    namespace: "position",
+    event_type: "message",
+    data: {
+      x: Game.player.sprite.x,
+      y: Game.player.sprite.y,
+      animation: Game.player.animation
+    }
+  };
+
+  // Sending via websocket
+  Game.ws.send(JSON.stringify(position));
 };
 
 const gameLoop = () => {
   tickerTime += 1 + Game.display.app.ticker.deltaMS;
 
+  // Every 1/100s
   if (tickerTime > 10) {
     tickerTime = 0;
+
+    // Send the player position if he moved
     sendPosition();
   }
+
+  // Moving the player on screen
   Game.player.sprite.x += Game.player.sprite.vx;
   Game.player.sprite.y += Game.player.sprite.vy;
 };
 
+// Loading all resources and add the gameloop in the ticker.
 export function loadResources() {
   const characters = {
     name: "character",
@@ -55,8 +65,10 @@ export function loadResources() {
     Game.logIt("Assets loaded.");
     Game.loaded = true;
 
+    // Setup the game (load player etc.)
     Game.setup();
 
+    // Add a ticker
     Game.display.app.ticker.add(() => gameLoop());
   });
 }
