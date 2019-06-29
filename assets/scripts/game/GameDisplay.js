5e86f8547caa3149b57d1f4ae9d7ba71506bd908
Kevin Kuipers <nanark@gmail.com>
2019-06-29 12:31:10 +0200

    Resize renderer with window.

--- assets/scripts/game/GameDisplay.js
+++ assets/scripts/game/GameDisplay.js
@@ -11,18 +11,29 @@ export default class GameDisplay {
 
     // Creating the application
     this.app = new PIXI.Application({
-      width: 250,
-      height: 250,
       antialias: false,
+      autoDensity: true,
+      height: 250,
+      powerPreference: "high-performance",
+      resolution: window.devicePixelRatio,
       transparent: true,
-      powerPreference: "high-performance"
+      // backgroundColor: 0x061639,
+      width: 250
     });
+
+    const resize = () => {
+      const _w = window.innerWidth;
+      const _h = window.innerHeight;
+      this.app.renderer.resize(_w, _h);
+    };
+
+    addEventListener("resize", resize);
 
     // Styles for fullscreen
     this.app.renderer.view.style.position = "absolute";
     this.app.renderer.view.style.display = "block";
     this.app.renderer.autoResize = true;
-    this.app.renderer.resize(window.innerWidth, window.innerHeight);
+    resize();
 
     // Load resources and start loop
     loadResources();
