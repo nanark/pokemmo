0345224a2be664d749e2d907196f7f701937c66a
Kevin Kuipers <nanark@gmail.com>
2019-06-29 18:40:28 +0200

    Debug mode

--- assets/scripts/game/GameLoop.js
+++ assets/scripts/game/GameLoop.js
@@ -4,6 +4,7 @@ import { Game } from "@/assets/scripts/game/Game";
 
 let tickerTime = 0;
 let previousPosition = [0, 0];
+let previousPositionDebug = [0, 0];
 
 // Send the player position if he moved
 const sendPosition = () => {
@@ -42,7 +43,19 @@ const gameLoop = delta => {
     }
 
     if (Game.debugMode) {
-      console.log(`Delta: ${delta}`);
+      const currentPosition = [Game.player.sprite.x, Game.player.sprite.y];
+      const differencePosition = [
+        Math.abs(~~(currentPosition[0] - previousPositionDebug[0])),
+        Math.abs(~~(currentPosition[1] - previousPositionDebug[1]))
+      ];
+
+      previousPositionDebug = currentPosition;
+
+      console.log(
+        `Delta: ${delta} - Movement: ${differencePosition[0]}x${
+          differencePosition[1]
+        }px`
+      );
     }
   }
 
