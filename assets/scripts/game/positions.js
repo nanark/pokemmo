60b70d140e8a376f577a4fae6b17b6086cfc246a
Kevin Kuipers <nanark@gmail.com>
2019-07-07 12:10:50 +0200

    debug ms received

--- assets/scripts/game/positions.js
+++ assets/scripts/game/positions.js
@@ -1,11 +1,19 @@
 import { Game } from "@/assets/scripts/game/Game";
 import NPC from "@/assets/scripts/game/actors/NPC";
 
-let oldX = 0;
-let oldY = 0;
+// let oldX = 0;
+// let oldY = 0;
+let oldNow = 0;
 
 export const moveCharacters = positions => {
   positions.data.forEach(position => {
+    const now = Date.now();
+    const diff = now - oldNow;
+    if (diff > 40) {
+      console.log(now - oldNow);
+    }
+    oldNow = now;
+
     const x = position.x;
     const y = position.y;
     const userId = position.user.id;
@@ -16,16 +24,16 @@ export const moveCharacters = positions => {
       return false;
     }
 
-    if (Math.abs(oldX - x) > 7) {
-      console.log(`Hey issue X ${Math.abs(oldX - x)}`);
-    }
+    // if (Math.abs(oldX - x) > 7) {
+    //   console.log(`Hey issue X ${Math.abs(oldX - x)}`);
+    // }
 
-    if (Math.abs(oldY - y) > 7) {
-      console.log(`Hey issue Y ${Math.abs(oldY - y)}`);
-    }
+    // if (Math.abs(oldY - y) > 7) {
+    //   console.log(`Hey issue Y ${Math.abs(oldY - y)}`);
+    // }
 
-    oldX = x;
-    oldY = y;
+    // oldX = x;
+    // oldY = y;
 
     if (!Game.population[userId]) {
       Game.logIt("Generate a new NPC:" + username);
